b333f7adc73a93b4a71dfe6b161c12b9e7989b70
xxMrPHDxx <pian.minecraft1@gmail.com>
2018-05-09 04:26:23 +0800

    new commit

--- bot.js
+++ bot.js
@@ -1,26 +1,30 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
+
+bot.on('ready', (evt) => {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+bot.on('message', (user, userID, channelID, message, evt) => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '/') {
+    if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
@@ -37,4 +41,3 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
-bot.connect();
